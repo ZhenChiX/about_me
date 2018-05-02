4ade20d58299bce488df90992fffb45cf7ad7f1b
StarGale <zhenchix@hotmail.com>
2018-05-01 17:46:21 -0700

    barebone

--- js/app.js
+++ js/app.js
@@ -4,9 +4,9 @@
 var userName = prompt('Hi! What is your name!');
 
 // Greet the user by name in an alert
-alert('Hi,'+ userName + ' thanks for stopping by');
+alert('Hi,' + userName + ' thanks for stopping by');
 // Log that to the console
-console.log('The user said their name is'+ userName);
+console.log('The user said their name is' + userName);
 
 // Ask the first guessing game question
 var answer1 = prompt('Do you thing I have any sibling?');
@@ -18,13 +18,54 @@ var answer1 = prompt('Do you thing I have any sibling?');
 // } else{
 //     do this
 // }
-if(answer1 === 'yes' && answer1 ==='y'){
-// right answer
-alert('You got it right!!!');
-console.log('The user got the correct answer(yes)');
-//  wrong answer
-alert('Fail');
-console.log('The user got the wrong answer(No)');
+if (answer1 === 'yes' || answer1 === 'y') {
+	// right answer
+	alert('Unfortunately I was born under the one child policy era, I always want to have one tho');
+	console.log('The user got the correct answer to #1 (NOPE, I have a sibling)');
+} else {
+	// wrong answer
+	alert('Wish I have one!');
+	console.log('The user got the wrong answer to #1 (Yup,I am sibling-free)');
 }
 // Tell user if they are wrong or right
 // Next question
+// var answer2 = prompt('');
+// if (answer2 === 'yes' || answer2 === 'y') {
+// 	// right answer
+// 	alert('YUP');
+// 	console.log('The user got the correct answer to #2');
+// } else {
+// 	//wrong answer
+// 	alert('Nah');
+// 	console.log('The user got the wrong answer to #2');
+// }
+var answer2 = prompt('I once spend entire night at Vegas only play slot machine');
+if (answer2 === 'yes' || answer2 === 'y') {
+	// right answer
+	alert('I lose quite a lot that night');
+	console.log('The user got the correct answer to #2');
+} else {
+	//wrong answer
+	alert('I have learned my lesson');
+	console.log('The user got the wrong answer to #2');
+}
+var answer3 = prompt('I particiated seattle color run last year');
+if (answer3 === 'yes' || answer3 === 'y') {
+	// right answer
+	alert('3 miles was not that hard,right');
+	console.log('The user got the correct answer to #3');
+} else {
+	//wrong answer
+	alert('3 miles was not that hard,right');
+	console.log('The user got the wrong answer to #3');
+}
+var answer4 = prompt('As a chinese myself,do you think I like fortune cookie?');
+if (answer4 === 'yes' || answer4 === 'y') {
+	// right answer
+	alert('What is FORTUNE COOKIE!?');
+	console.log('The user got the correct answer to #4');
+} else {
+	//wrong answer
+	alert('IT IS NOT EVEN A THING!');
+	console.log('The user got the wrong answer to #4');
+}
